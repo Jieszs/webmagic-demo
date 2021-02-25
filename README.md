03b77a00c42322658e48fe4ae8176bdca8ccda18
zhengjie <601116847@qq.com>
2021-02-25 10:03:22 +0800

    文档说明

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # webmagic-demo
-webmagic 0.74 爬虫示例
+webmagic 0.7.4 爬虫示例
+1.ZhiChiKeFuListProcessor 模拟ajax的get请求获取列表
+2.ZhiChiKeFuDeleteProcessor 模拟ajax的post请求删除操作
+3.AnJuKeProcessor 最简单的爬取静态页面
+4.QiuShiBaiKeProcessor 爬取静态页面文本结果，保存到本地
+5.ZipCodePageProcessor 爬取两层静态页面文本结果
 
--- src/main/java/samples/ajax/get/ZhiChiKeFuListProcessor.java
+++ src/main/java/samples/ajax/get/ZhiChiKeFuListProcessor.java
@@ -1,26 +1,19 @@
-package samples.ajax.post;
+package samples.ajax.get;
 
 import us.codecraft.webmagic.Page;
-import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
-import us.codecraft.webmagic.model.HttpRequestBody;
 import us.codecraft.webmagic.processor.PageProcessor;
-import us.codecraft.webmagic.utils.HttpConstant;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
- * 删除智齿客服的客户列表中的某个客户
- * <p>
+ * 获取智齿客服的客户列表
+ *
  * 先完成登录，再从f12开发者工具中获取temp-id
  * Date: 21-2-24
  * Time: 下午5:31
- *
  * @author qq3434569
  */
-public class ZhiChiKeFuProcessor implements PageProcessor {
+public class ZhiChiKeFuListProcessor implements PageProcessor {
     @Override
     public void process(Page page) {
         System.out.println(page.getRawText());
@@ -37,11 +30,6 @@ public class ZhiChiKeFuProcessor implements PageProcessor {
     }
 
     public static void main(String[] args) {
-        Request request = new Request("https://www.sobot.com/basic/deleteUserInfo/4");
-        request.setMethod(HttpConstant.Method.POST);
-        Map<String, Object> params = new HashMap<>();
-        params.put("id","e1eafd6751084442b00eedf31919d4f2");
-        request.setRequestBody(HttpRequestBody.form(params,"utf-8"));
-        Spider.create(new ZhiChiKeFuProcessor()).addRequest(request).run();
+        Spider.create(new ZhiChiKeFuListProcessor()).addUrl("https://www.sobot.com/basic/getUserList/4?pageNo=1&pageSize=15&queryContent=&queryType=5&userStatus=0").run();
     }
 }
